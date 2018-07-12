a879ac6bdd85d6f94db3f9dca86a6407a985e2b5
Phuong H <phuonghqh@gmail.com>
2018-07-12 21:59:19 +0700

    Fix for electron

--- binding.gyp
+++ binding.gyp
@@ -115,7 +115,7 @@
                   ],
                   'libraries': [
                     '-L<(javalibdir)',
-                    '-Wl,<(javalibdir)',
+                    '-Wl,-rpath,<(javalibdir)',
                     '-ljvm'
                   ],
                 },
--- findJavaHome.js
+++ findJavaHome.js
@@ -1,7 +1,10 @@
 require('find-java-home')(function(err, home){
-  if(err){
-    console.error("[node-java] "+err);
-    process.exit(1);
-  }
-  process.stdout.write(home);
+  // if(err){
+  //   console.error("[node-java] "+err);
+  //   process.exit(1);
+  // }
+  // process.stdout.write(home);
+  process.stdout.write('jdk/jre-9.0.4.jre/Contents/Home');
 });
+
+// process.stdout.write('jdk/jre-9.0.4.jre/Contents/Home');
